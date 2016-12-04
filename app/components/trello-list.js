c949df077e04d321d3b1a46cc68a89e6793a8f4b
Sebastian Higuita Castañeda <sebastianhc1995@hotmail.com>
2016-12-04 07:26:31 -0500

    trello emberjs

---
+++ app/components/trello-list.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    actions: {
+        delete() {
+            this.$().slideUp('normal', () => {
+                this.list.deleteRecord();
+                this.list.save();
+            });
+        },
+        addItem() {
+            this.sendAction('action', this.itemDescription, this.list);
+        }
+    }
+});
---
+++ app/models/item.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    description: DS.attr("string"),
+    list: DS.belongsTo("list")
+});
